465567da55f0efccc5367c8cfdc1c5242bb07edb
joykind <69032620+killer7877@users.noreply.github.com>
2023-09-29 22:55:24 +0900

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.BaseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.github.Aliucord:gradle:main-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+        maven("https://jitpack.io")
+    }
+}
+fun Project.aliucord(
+        configuration: AliucordExtension.() -> Unit
+) = extensions.getByName<AliucordExtension>("aliucord").configuration()
+
+fun Project.android(
+        configuration: BaseExtension.() -> Unit
+) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "com.aliucord.gradle")
+    apply(plugin = "kotlin-android")
+
+    aliucord {
+        val baseGithubUrl = "https://raw.githubusercontent.com/killer7877/AliucordPlugins"
+        author("killer7877", 794527403580981248L)
+        updateUrl.set("$baseGithubUrl/builds/updater.json")
+        buildUrl.set("$baseGithubUrl/builds/%s.zip")
+    }
+
+    android {
+        compileSdkVersion(30)
+
+        defaultConfig {
+            minSdk = 24
+            targetSdk= 30
+            versionCode = 1
+            versionName = "1.0"
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        tasks.withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+
+    dependencies {
+        val discord by configurations
+        val implementation by configurations
+
+        discord("com.discord:discord:aliucord-SNAPSHOT")
+        implementation("com.aliucord:Aliucord:main-SNAPSHOT")
+
+        implementation("androidx.appcompat:appcompat:1.4.0")
+        implementation("com.google.android.material:material:1.4.0")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
